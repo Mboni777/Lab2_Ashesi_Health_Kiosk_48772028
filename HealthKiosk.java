64ce91926f5e8d2b2ef6a6a59e3ada899871efa6
Mboni777 <mboninkunda@gmail.com>
2025-09-28 14:06:44 +0000

    Project setup and Giving a welcome message

---
+++ HealthKiosk.java
@@ -0,0 +1,62 @@
+
+// Task 0: Setting up project structure
+import java.util.Scanner;
+
+public class HealthKiosk {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        // printing a welcome message
+        System.out.println("Welcome to the Ashesi Health Kiosk!");
+
+
+
+
+
+
+    
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
